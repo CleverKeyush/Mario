6b7b09782371dd42dc56c3e6d61d20aba717455b
Soham <110964040+Kenmaaa05@users.noreply.github.com>
2024-10-07 19:41:35 +0530

    Update enemypcg.cs

--- Assets/Scripts/enemypcg.cs
+++ Assets/Scripts/enemypcg.cs
@@ -14,7 +14,7 @@ public class enemypcg : MonoBehaviour
 
         for (int i = 0; i < numberOfInstances; i++)
         {
-            float randomX = Random.Range(15f, 250); // Random X position between 0 and 300
+            float randomX = Random.Range(15f, 250f); // Random X position between 0 and 300
             float randomY = Random.Range(-2f, 4f);  // Generate a random Y value
 
             position.x = randomX; // Set the X position to the random value
